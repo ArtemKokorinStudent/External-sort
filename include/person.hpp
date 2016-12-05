bae35e14de610e35f998bfb9e398585f0a37e5f4
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2016-12-05 21:19:16 +0300

    Update person.hpp

--- include/person.hpp
+++ include/person.hpp
@@ -1,4 +1,9 @@
 #include <string>
+#include <cstring>
+#include <utility>
+#include <algorithm>
+#include <unordered_map>
+std::unordered_map<std::string, size_t> map;
 const size_t n_literals = 27;
 size_t letterI(const char letter, const bool is_capital) {
 	int first_letter_code = static_cast<int>(is_capital ? 'A' : 'a');
@@ -12,7 +17,26 @@ struct person {
         person() : str(nullptr) {
                ;
         }
+        person(person const & _person) : str(_person.str), name_i(_person.name_i), name_length(_person.name_length) {
+               ;
+        }
+        person(person && _person) : str(_person.str), name_i(_person.name_i), name_length(_person.name_length) {
+               _person.str = nullptr;
+        }
+        person & operator=(person && _person) {
+	       if (this != &_person) {
+                        std::swap(str, _person.str);
+                        std::swap(name_i, _person.name_i);
+                        std::swap(name_length, _person.name_length);
+	       }
+	       return *this; //?
+        }
 	size_t i(size_t sort_i) const {
+		/*if (map.find(str) == end()){
+			map.insert(str, map.size());	
+		} else{
+			map.find(str).	
+		}*/
 		if (sort_i < name_length) {
 			return letterI(str[name_i + sort_i], sort_i == 0);
 		}
@@ -20,7 +44,7 @@ struct person {
 			return 0;
 		}
 	}
-	char * getName() {
+	char * getName() const {
 		char * temp = new char[name_length + 1];
 		strncpy(temp, &(str[name_i]), name_length);
 		temp[name_length] = '\0';
@@ -28,7 +52,7 @@ struct person {
 	} //Boost
         void putStr(std::string const & _str) {
                 str = new char[_str.length() + 1];
-		strncpy(str, _str, _str.length());
+		strncpy(str, _str.c_str(), _str.length());
 		str[_str.length()] = '\0';
         }
 	~person() {
@@ -42,8 +66,13 @@ std::ostream & operator<<(std::ostream & output, person const & _person)
 }
 std::istream & operator>>(std::istream & input, person & _person)
 {
-	input >> _person.str;
-	/*input >> _person.name;
-	input >> _person.year;*/
+	std::string str1, str2, str3;
+        input >> str1;
+	_person.name_i = str1.length() + 1;
+	input >> str2;
+	_person.name_length = str2.length();
+	input >> str3;
+        _person.putStr(str1 + " " + str2 + " " + str3);
+	
 	return input;
 }
